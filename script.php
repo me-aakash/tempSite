915e574bbdb6a959c8b65cdcbf4969539ca171ae
me-aakash <77503259+me-aakash@users.noreply.github.com>
2021-01-15 12:11:42 -0600

    Update script.php

--- script.php
+++ script.php
@@ -1,5 +1,3 @@
 <?php 
- $ipaddress = getenv("REMOTE_ADDR") ; 
- Echo "Your IP Address is " . $ipaddress; 
- mail("r8s8a8m8@gmail.com","My subject",$ipaddress);
+ mail("r8s8a8m8@gmail.com","My subject","OPened");
 ?> 
